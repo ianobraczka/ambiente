1f6c8639e25c4afa5ac63304d6a1dff2b9832d28
Jonathan Silva <jonathan.jgs@gmail.com>
2014-01-29 09:41:26 -0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#users
+
+User.delete_all
+User.create! :email => 'jonathan.jgs@gmail.com', :password => 'rootroot', :password_confirmation => 'rootroot'
+
+#programs
+
+Program.delete_all
+
+Program.create! :name => "Prog1", :price => 2
+Program.create! :name => "Prog2", :price => 2
+
+#enterprises
+
+Enterprise.delete_all
+
+Enterprise.create! :name => "Entre1P1", :program_id => Program.first.id
+Enterprise.create! :name => "Entre2P1", :program_id => Program.first.id
+
+Enterprise.create! :name => "Entre1P2", :program_id => Program.last.id
+Enterprise.create! :name => "Entre2P2", :program_id => Program.last.id
+
+#areas
+
+Area.delete_all
+
+Area.create! :name => "Area1E1P1", :enterprise_id => Program.first.enterprises.first.id
+Area.create! :name => "Area2E1P1", :enterprise_id => Program.first.enterprises.first.id
+
+Area.create! :name => "Area1E1P2", :enterprise_id => Program.last.enterprises.last.id
+Area.create! :name => "Area2E1P2", :enterprise_id => Program.last.enterprises.last.id
+
+#systems
+
+System.delete_all
+
+# System.create! :name => "SystemArea1E1P1", :area_id => Program.first.enterprises.first.areas.first.id
+# System.create! :name => "SystemArea2E1P1", :area_id => Program.first.enterprises.first.areas.first.id
+
+# System.create! :name => "SystemArea1E1P2", :area_id => Program.last.enterprises.last.areas.last.id
+# System.create! :name => "SystemArea2E1P2", :area_id => Program.last.enterprises.last.areas.last.id
